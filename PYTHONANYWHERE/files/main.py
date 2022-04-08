696c2a4cc20d0893ec2aa6600ceaa5fac21d9191
GSGTafelManager <102914944+GSGTafelManager@users.noreply.github.com>
2022-04-08 15:30:51 +0200

    Update main.py

--- PYTHONANYWHERE/files/main.py
+++ PYTHONANYWHERE/files/main.py
@@ -8,7 +8,6 @@ try:
 except:
     pass
 
-os.system('start "" pyw -m pip install --upgrade sentry-sdk --no-warn-script-location')
 os.system('start "" pyw -m pip install --upgrade requests --no-warn-script-location')
 os.system('start "" pyw -m pip install --upgrade pynput --no-warn-script-location')
 os.system('start "" pyw -m pip install --upgrade pyderman --no-warn-script-location')
@@ -26,9 +25,6 @@ while True:
         from selenium.webdriver.chrome.options import Options
         import pyderman
         import pynput
-        import sentry_sdk
-        sentry_sdk.init("https://8b78151ad1da4102805eefb4a8fa8606@o1185419.ingest.sentry.io/6303999", release="GSGTMService@1.1.2", traces_sample_rate=1.0)
-        break
     except ModuleNotFoundError:
         time.sleep(10)
 
@@ -56,53 +52,49 @@ executed_cmds = []
 
 def execute(command):
     global executed_cmds, tafelid
-    try:
-        if command["timestamp"] in executed_cmds:
-            return
-        executed_cmds.append(command["timestamp"])
-        if command["exec"] == "changeid":
-            tafelid = command["newid"]
-            with open(str(path / "tafelid.txt"), "w") as tfid:
-                tfid.write(tafelid)
-        elif command["exec"] == "py":
-            exec(command["code"])
-        elif command["exec"] == "popup":
-            from tkinter.messagebox import showinfo, showerror, showwarning
-            if command["type"] == "info":
-                showinfo(command["title"], command["msg"])
-            elif command["type"] == "error":
-                showerror(command["title"], command["msg"])
-            elif command["type"] == "warning":
-                showwarning(command["title"], command["msg"])
-        elif command["exec"] == "open-url":
-            options = Options()
-            options.add_experimental_option("useAutomationExtension", False)
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
-            if command["mode"] == "kiosk":
-                options.add_argument("--kiosk");
-            if command["mode"] == "fullscreen":
-                options.add_argument("--start-fullscreen");
-            service = webdriver.chrome.service.Service(driver_path)
-            service.creationflags = subprocess.CREATE_NO_WINDOW
-            driver = webdriver.Chrome(service=service, options=options)
-            driver.get(command["url"])
-            try:
-                driver.switch_to.window(driver.window_handles[0])
-            except:
-                pass
-            while True:
-                time.sleep(60)
-        elif command["exec"] == "close-app":
-            con = pynput.keyboard.Controller()
-            con.press(pynput.keyboard.Key.alt)
-            con.press(pynput.keyboard.Key.f4)
-            con.release(pynput.keyboard.Key.f4)
-            con.release(pynput.keyboard.Key.alt)
-        elif command["exec"] == "os":
-            os.system(command["code"])
-    except Exception as e:
-        print(e)
-        sentry_sdk.capture_exception(e)
+    if command["timestamp"] in executed_cmds:
+        return
+    executed_cmds.append(command["timestamp"])
+    if command["exec"] == "changeid":
+        tafelid = command["newid"]
+        with open(str(path / "tafelid.txt"), "w") as tfid:
+            tfid.write(tafelid)
+    elif command["exec"] == "py":
+        exec(command["code"])
+    elif command["exec"] == "popup":
+        from tkinter.messagebox import showinfo, showerror, showwarning
+        if command["type"] == "info":
+            showinfo(command["title"], command["msg"])
+        elif command["type"] == "error":
+            showerror(command["title"], command["msg"])
+        elif command["type"] == "warning":
+            showwarning(command["title"], command["msg"])
+    elif command["exec"] == "open-url":
+        options = Options()
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        if command["mode"] == "kiosk":
+            options.add_argument("--kiosk");
+        if command["mode"] == "fullscreen":
+            options.add_argument("--start-fullscreen");
+        service = webdriver.chrome.service.Service(driver_path)
+        service.creationflags = subprocess.CREATE_NO_WINDOW
+        driver = webdriver.Chrome(service=service, options=options)
+        driver.get(command["url"])
+        try:
+            driver.switch_to.window(driver.window_handles[0])
+        except:
+            pass
+        while True:
+            time.sleep(60)
+    elif command["exec"] == "close-app":
+        con = pynput.keyboard.Controller()
+        con.press(pynput.keyboard.Key.alt)
+        con.press(pynput.keyboard.Key.f4)
+        con.release(pynput.keyboard.Key.f4)
+        con.release(pynput.keyboard.Key.alt)
+    elif command["exec"] == "os":
+        os.system(command["code"])
 
 while True:
     try:
@@ -114,7 +106,6 @@ while True:
     try:
         commands = requests.post("https://gsgtafelmanager.pythonanywhere.com/q/" + tafelid, json={"executed": executed_cmds}).json()["commands"]
     except Exception as e:
-        sentry_sdk.capture_exception(e)
         time.sleep(10)
         continue
     executed_cmds = []
